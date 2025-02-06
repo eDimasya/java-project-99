fc6ad765de95a849f5338171d02f14ed02cb7a6f
Dima <edimasya@gmail.com>
2025-02-07 00:34:18 +0100

    Added Checkstyle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,11 @@ plugins {
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
 	id("gg.jte.gradle") version "3.1.12"
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.51.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    checkstyle
+    jacoco
 }
 
 group = "hexlet.code"
@@ -34,7 +39,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("gg.jte:jte:3.1.12")
 	implementation("gg.jte:jte-spring-boot-starter-3:3.1.12")
-	compileOnly("org.projectlombok:lombok")
+	//compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("org.postgresql:postgresql")
@@ -43,6 +48,14 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.jacoco:org.jacoco.core:0.8.11")
+
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+    testCompileOnly("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
 }
 
 jte {
@@ -50,6 +63,24 @@ jte {
 	binaryStaticContent = true
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required = true
+        html.outputLocation = layout.buildDirectory.dir("reports/jacoco/test/html")
+    }
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+    reportsDirectory = layout.buildDirectory.dir("reports/jacoco")
+}
+
+checkstyle {
+    configFile = rootProject.file("config/checkstyle/checkstyle.xml")
 }
